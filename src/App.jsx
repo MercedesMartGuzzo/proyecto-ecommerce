781d03cc7028a3515d0a9f7085bb24a170216cc1
Mercedes Martinez <mercedesviolin@gmail.com>
2024-03-28 01:13:26 -0300

    Se crearon carpetas pages,common, components, layout y se crearon los componentes navba e itemlistcontainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Navbar } from "./components/layout/navbar/Navbar";
+import ItemListContainer from "./components/pages/itemListContainer/ItemListContainer.jsx";
+
 
 function App() {
-  return (
-  <h1>Hola</h1>
+
+  return  (<div>
+   <Navbar/>
+   <ItemListContainer gretting={"Bienvenidos a nuestra tienda!"}/>
+    </div>
   )
 }
 
---
+++ src/components/layout/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+export const Navbar = () => {
+
+  return (
+    <ul>
+        <li>Inicio</li>
+        <li>Intrumentos</li>
+        <li>Cuerdas</li>
+        <li>Accesorios</li>
+        <li>Estuches</li>
+    </ul>
+  )
+};
