d4dbbd7bb53a7f348298c99f3469ebe613f8c123
Mercedes Martinez <mercedesviolin@gmail.com>
2024-03-31 22:57:49 -0300

    Pre entrega Mercedes Martinez, se agrega las etiquetas a en el archivo navbar.jsx y se le da estilo en el archivo index.css

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -2,18 +2,20 @@ import "./Navbar.css";
 import { AppBar, Box, Badge } from "@mui/material";
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
+
 export const Navbar = () => {
   return (
-    <Box>
-      <AppBar sx={{ backgroundColor: "peru"}}>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar sx={{ backgroundColor: "peru", position: "static"}}>
+
         <ul className="lista">
           <li>
             <h2 className="logo">VIOLIN SHOP</h2>
           </li>
-          <li>Intrumentos</li>
-          <li>Cuerdas</li>
-          <li>Accesorios</li>
-          <li>Estuches</li>
+          <li><a href="">Intrumentos</a></li>
+          <li><a href="">Cuerdas</a></li>
+          <li><a href="">Accesorios</a></li>
+          <li><a href="">Estuches</a></li>
          <li> <Badge badgeContent={0} color="primary" showZero={true}><AddShoppingCartIcon/></Badge></li>
         </ul>
       </AppBar>
